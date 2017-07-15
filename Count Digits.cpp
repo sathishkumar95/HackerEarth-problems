d12ddb6fc1e9e7f810d841324a8bae9ef21b74e7
P SATHISH KUMAR <sathishp.railway@gmail.com>
2017-07-15 10:27:11 +0530

    Update Count Digits.cpp

--- Count Digits.cpp
+++ Count Digits.cpp
@@ -1,3 +1,18 @@
+/*
+PROBLEM
+You are given a string S. Count the number of occurrences of all the digits in the string S.
+
+Input: 
+First line contains string S.
+
+Output: 
+For each digit starting from 0 to 9 print the count of their occurrences in the string S. So, print 10 lines, each line containing 
+2 space separated integers. First integer i and second integer count of occurrence of i. See sample output for clarification.
+
+Constraints: 1 ≤ |S| ≤ 100
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
